68d005c8c15da8b9778c720edc0696837dac80c4
Akiu1996 <57586401+Akiu1996@users.noreply.github.com>
2020-01-19 19:14:41 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,7 +22,6 @@ $(function() {
           html += `</div></div>`
           return html;
   };
-    
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
