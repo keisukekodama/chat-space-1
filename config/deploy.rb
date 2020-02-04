597fb36e3f0999bf0ce897b2a96baf62d9d192f4
keisukekodama <myzk.kodama@gmail.com>
2020-02-04 20:43:23 +0900

    Update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
 lock '3.11.2'
 
-set :application, 'chat-space'
-set :repo_url,  'git@github.com:Akiu1996/chat-space.git'
+set :application, 'chat-space-1'
+set :repo_url,  'git@github.com:keisukekodama/chat-space-1.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/akeu2020.pem'] 
+                  keys: ['~/.ssh/chatspace.pem'] 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,4 +60,4 @@
 #     # password: "please use keys"
 #   }
 
-server '13.112.65.96', user: 'ec2-user', roles: %w{app db web}
+server '52.192.58.131', user: 'ec2-user', roles: %w{app db web}
